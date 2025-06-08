4b0b159d162ce5bdc1e9e24300ec3ad5cfc70e65
P. Kinzerskiy <pkinzerskiy@gmail.com>
2025-06-08 07:25:51 +0300

    begining rough work with button

--- src/js/artists/artists-render.js
+++ src/js/artists/artists-render.js
@@ -10,7 +10,13 @@ export function markupCardArtist(date) {
                 <ul class="art-genre-list"><li class="art-genre-list-item">${genres.join( ).replaceAll(",",'</li><li class="art-genre-list-item">')}</li></ul>             
                 <h4 class="art-list-name">${strArtist}</h4>
                 <p class="art-list-biography">${strBiographyEN.slice(0, 145)}...</p>
-              <button type="button" class="art-btn-learnMore"> Learn More </button>  
+              <button type="button" class="art-btn-learnMore"> Learn More
+              
+              <svg class="icon" width="24" height="24">
+                        <use
+                          href="./../../img/symbol-defs.svg#icon-caret-right"
+                        ></use></svg>
+              </button>  
      `).join('');
             
     // console.log('markup:', markup);
