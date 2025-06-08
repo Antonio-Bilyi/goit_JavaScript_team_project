e2bf456b0202a86805f637502d4deb21f7d3e92d
P. Kinzerskiy <pkinzerskiy@gmail.com>
2025-06-08 11:07:48 +0300

    artists markup with buttin, mob version

--- src/js/artists/artists-render.js
+++ src/js/artists/artists-render.js
@@ -2,23 +2,25 @@
 
 const artistsContainer = document.querySelector(".art-list-card");
 
-export function markupCardArtist(date) {
-    let markup = date.map(
-        ({ strArtist, strBiographyEN, strArtistThumb, genres }) => 
-                `<li class="art-item">
-                <img class="art-img" src="${strArtistThumb}" alt="${strArtist}" />
-                <ul class="art-genre-list"><li class="art-genre-list-item">${genres.join( ).replaceAll(",",'</li><li class="art-genre-list-item">')}</li></ul>             
-                <h4 class="art-list-name">${strArtist}</h4>
-                <p class="art-list-biography">${strBiographyEN.slice(0, 145)}...</p>
-              <button type="button" class="art-btn-learnMore"> Learn More
-              
-              <svg class="icon" width="24" height="24">
-                        <use
-                          href="./../../img/symbol-defs.svg#icon-caret-right"
-                        ></use></svg>
-              </button>  
-     `).join('');
-            
-    // console.log('markup:', markup);
+export function markupCardArtist(data) {
+    let widthWindows = document.documentElement.clientWidth;
+    let artTextRow = widthWindows <= 768 ? 50 : 145;
+
+    const markup = data.map(({ strArtist, strBiographyEN, strArtistThumb, genres }) => `
+      <li class="art-item">
+        <img class="art-img" src="${strArtistThumb}" alt="${strArtist}" />
+        <ul class="art-genre-list">
+          ${genres.map(genre => `<li class="art-genre-list-item">${genre}</li>`).join('')}
+        </ul>
+        <h4 class="art-list-name">${strArtist}</h4>
+        <p class="art-list-biography">${strBiographyEN.slice(0, artTextRow)}...</p>
+        <button type="button" class="art-btn-learnMore">Learn More
+          <svg class="icon" width="24" height="24">
+            <use href="./../../img/symbol-defs.svg#icon-caret-right"></use>
+          </svg>
+        </button>
+      </li>
+    `).join('');
     artistsContainer.insertAdjacentHTML("beforeend", markup);
-}
+  }
+  
