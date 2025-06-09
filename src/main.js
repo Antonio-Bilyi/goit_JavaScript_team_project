8e5cafcd258e042ad82483bbb46e8705067ca23c
Anton Bilyi <anthonybelyj@protonmail.com>
2025-06-09 12:40:03 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './js/burger-menu.js';
+import '/js/burger-menu.js';
 import './js/artists/main-artists.js';
 import './js/artists-details/main-artist-details.js';
 import './js/feedback/main-feedback.js';
