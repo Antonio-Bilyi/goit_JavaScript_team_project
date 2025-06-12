eaaa5f49f92c56712683abe59b02d559f0f76e42
ShumyloYana <zolotushko1@gmail.com>
2025-06-12 17:30:03 +0300

    changes after the mentor

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import './js/burger-menu.js';
 import './js/artists/main-artists.js';
 import './js/artists-details/main-artist-details.js';
 import './js/feedback/main-feedback.js';
-import './js/hero-btn.js';
+// import './js/hero-btn.js';
 
-import { initFeedbackSlider } from './js/feedback/main-feedback.js';
+// import { initFeedbackSlider } from './js/feedback/main-feedback.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initFeedbackSlider();
